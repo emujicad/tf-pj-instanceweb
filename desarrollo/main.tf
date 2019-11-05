a1b868ca332b99c09bbb662e765ed247e67247a8
Ender Mujica <emujicad@gmail.com>
2019-11-04 23:01:26 -0300

    fixed main.tf files

--- desarrollo/main.tf
+++ desarrollo/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
    #source = "C:\\Users\\user\\Documents\\terraform\\terraform-modules\\ec2-eip"
-   source = "github.com/emujicad/terraform-modules/ec2-eip"
+   source = "github.com/emujicad/tf-mod-ec2-eip"
    project_name  = "tf"
    instance_type = "t2.micro"
    workspace   = "desarollo"
